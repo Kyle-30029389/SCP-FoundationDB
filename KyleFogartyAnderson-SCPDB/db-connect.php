5a180e607efd6285c2080395168db5e251cf6f7f
fogartyy <kmaster1098765@gmail.com>
2020-06-19 13:48:26 +1200

    SCPDB

--- KyleFogartyAnderson-SCPDB/db-connect.php
+++ KyleFogartyAnderson-SCPDB/db-connect.php
@@ -1,6 +1,6 @@
 <?php
     $link = mysqli_connect( 'localhost', 
-                            'user', 
+                            'a3002938_SCP', 
                             'p*1Ku?25g,Yy', 
                             'a3002938_SCPData' ); 
 ?>
